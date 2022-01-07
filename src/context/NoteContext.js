49bcd49828023ba36cf273a2ba25bfaeaa9f4c9b
sametkoyuncu <sametkoyuncu@live.com>
2022-01-07 18:16:42 +0300

    Add context

---
+++ src/context/NoteContext.js
@@ -0,0 +1,42 @@
+import { createContext, useContext, useEffect, useState } from 'react'
+
+const NoteContext = createContext()
+
+const defaultNotes = [
+  {
+    id: 1,
+    title: 'Note One',
+    text: 'Lorem ipsum dolor sit amet.',
+  },
+  {
+    id: 2,
+    title: 'Note Two',
+    text: 'Ipsun lorem dolor sit amet.',
+  },
+  {
+    id: 3,
+    title: 'Note Three',
+    text: 'Dolor lorem ipsum sit amet.',
+  },
+  {
+    id: 4,
+    title: 'Note Four',
+    text: 'Site lorem ipsum dolor amet.',
+  },
+]
+
+export const NoteProvider = ({ children }) => {
+  const [notes, setNotes] = useState(
+    localStorage.getItem('notes') || defaultNotes
+  )
+  useEffect(() => {
+    localStorage.setItem('notes', notes)
+  }, [notes])
+  const values = {
+    notes,
+    setNotes,
+  }
+  return <NoteContext.Provider value={values}>{children}</NoteContext.Provider>
+}
+
+export const useNote = () => useContext(NoteContext)
