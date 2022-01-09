db34340470c4837351c7ee522ce1789de565c274
sametkoyuncu <sametkoyuncu@live.com>
2022-01-09 21:57:02 +0300

    Add 'add note btn'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import NoteCard from './components/NoteCard'
 import EditModal from './components/EditModal'
+import AddModal from './components/AddModal'
 import { useNote } from './context/NoteContext'
 import './App.css'
+import AddNoteBtn from './components/AddNoteBtn'
 
 function App() {
   const [modal, setModal] = useState(false)
+  const [addModal, setAddModal] = useState(false)
   const [selectedNoteId, setSelectedNoteId] = useState(null)
   const [selectedNoteTitle, setSelectedNoteTitle] = useState(null)
   const [selectedNoteText, setSelectedNoteText] = useState(null)
@@ -33,6 +36,10 @@ function App() {
     }
   }
 
+  const showAddModal = () => {
+    setModal(true)
+  }
+
   const deleteNote = (id) => {
     let _notes = notes.filter((note) => note.id !== id)
     setNotes(_notes)
@@ -40,7 +47,11 @@ function App() {
 
   return (
     <div className="w-full min-h-screen bg-sky-300">
-      <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-4 pt-10">
+      <div className="container mx-auto px-4 pt-6">
+        <AddNoteBtn showAddModal={showAddModal} />
+      </div>
+
+      <div className="container mx-auto px-4 grid grid-cols-1 md:grid-cols-3 gap-4 pt-6">
         {notes.map((note) => (
           <NoteCard
             key={note.id}
@@ -51,6 +62,7 @@ function App() {
         ))}
       </div>
       {modal && <EditModal setModal={setModal} selectedNote={selectedNote} />}
+      {addModal && <AddModal setAddModal={setAddModal} />}
     </div>
   )
 }
